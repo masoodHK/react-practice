1d434a6818b2991f41a5be6dba0dbbb450f81b57
Masood Hasan Khan <masood_hasan@msn.com>
2018-08-31 21:43:16 +0500

    modified the file

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-class App extends Component{
+class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       input: "",
       reverseString: "",
-      textArray: [],
-    }
-    
+      textArray: []
+    };
+
     this.reverseInput = this.reverseInput.bind(this);
     this.addText = this.addText.bind(this);
     this.addText = this.addText.bind(this);
   }
   reverseInput(event) {
     this.setState({
-      input:event.target.value,
-      reverseString: event.target.value.split("").reverse().join("")
+      input: event.target.value,
+      reverseString: event.target.value
+        .split("")
+        .reverse()
+        .join("")
     });
   }
 
@@ -29,41 +32,47 @@ class App extends Component{
 
     textArray.push({
       input,
-      date: (new Date()).toLocaleTimeString() + " " + (new Date()).toLocaleDateString(),
-      isVisible: true,
+      date:
+        new Date().toLocaleTimeString() + " " + new Date().toLocaleDateString(),
+      isVisible: true
     });
 
-    this.setState({textArray})
+    this.setState({ textArray });
   }
 
   toggleVisibility(textIndex) {
     const { textArray } = this.state;
-    console.log(textArray)
     let UpdatedTextArray = textArray.map((text, index) => {
-      if(index === textIndex) {
-        text.isVisible = text.isVisible ? false: true
+      if (index === textIndex) {
+        text.isVisible = text.isVisible ? false : true;
       }
-      return text
-    })
+      return text;
+    });
 
-    this.setState({textArray: UpdatedTextArray})
+    this.setState({ textArray: UpdatedTextArray });
   }
 
   render() {
     const { textArray, input, reverseString } = this.state;
     return (
       <div className="App">
-        <input value={input} onChange={this.reverseInput}/>
-        <p>Normal String: {input}</p>
-        <p>Reverse String: {reverseString}</p>
+        <input value={input} onChange={this.reverseInput} />
+        {/*<p>Normal String: {input}</p>*/}
+        {/*<p>Reverse String: {reverseString}</p>*/}
 
         <button onClick={this.addText}>Add Text</button>
         <ul>
           {textArray.map((text, index) => {
-            return <li key={index}>
-              <span className={text.isVisible? "visible": "invisible"}>{text.input} Created at: {text.date} </span>
-                <button onClick={() => this.toggleVisibility(index)}>{text.isVisible? "Hide": "Show"}</button>
+            return (
+              <li key={index}>
+                <span className={text.isVisible ? "visible" : "invisible"}>
+                  {text.input} Created at: {text.date}{" "}
+                </span>
+                <button onClick={() => this.toggleVisibility(index)}>
+                  {text.isVisible ? "Hide" : "Show"}
+                </button>
               </li>
+            );
           })}
         </ul>
       </div>
